2db9baedb7b9c2ad1134b7b7658bd5bd09205eba
izzMalakh <izzaldeenjamal@gmail.com>
2022-12-07 13:59:56 +0200

    -

---
+++ React/First JSX/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from './components/Index';
+
+function App() {
+  return (
+    <div>
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/First JSX/components/Index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div className='diva'>
+        <h1 id="izz">Hello Dojo</h1>
+        <h2> Things I need to do </h2>
+        <ul>
+            <li> i need to learn React </li>
+            <li> i want to eat something </li>
+            <li> i want to take a shower </li>
+            <li> i want to call rana </li>
+        </ul>
+      </div>
+    )
+  }
+}
